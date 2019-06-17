3921ca4d437ec661289aaf5a388fc65ee7e506b6
Andrey Lapin <lapinandrey95@gmail.com>
2019-06-17 19:09:10 -0400

    Fixed the error

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User');
+require('./models/user');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
