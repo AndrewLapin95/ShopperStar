c155c8272cb30251f0cb81e95c824beb6600dbd1
Andrey Lapin <lapinandrey95@gmail.com>
2019-06-20 20:09:57 -0400

    Started setting up the client side

---
+++ server/client/src/index.js
@@ -0,0 +1,32 @@
+import 'jquery';
+import Popper from 'popper.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import "./components/css/cart_responsive.css";
+import "./components/css/cart.css";
+import "./components/css/categories_responsive.css";
+import "./components/css/categories.css";
+import "./components/css/checkout_responsive.css";
+import "./components/css/checkout.css";
+import "./components/css/contact_responsive.css";
+import "./components/css/contact.css";
+import "./components/css/main_styles.css";
+import "./components/css/product_responsive.css";
+import "./components/css/product.css";
+import "./components/css/responsive.css";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, 
+    document.querySelector('#root')
+);
--- server/services/passport.js
+++ server/services/passport.js
@@ -20,16 +20,14 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then((user) => done(null, user));
-                }
-            });
+    }, async(accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
     })
 );
