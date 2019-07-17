30c8c89eb1d41bca10a28164da69a5762ec68b5d
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-16 21:13:30 -0400

    Made website responsive

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,20 +3,19 @@ import reduxThunk from 'redux-thunk';
 import Popper from 'popper.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import "./components/css/cart.css";
 import "./components/css/font-awesome.min.css";
 import "./components/css/categories.css";
 import "./components/css/checkout.css";
 import "./components/css/contact.css";
-import "./components/css/main_styles.css";
-import "./components/css/product.css";
-import "./components/css/product_responsive.css";
 import "./components/css/cart_responsive.css";
 import "./components/css/categories_responsive.css";
 import "./components/css/checkout_responsive.css";
 import "./components/css/contact_responsive.css";
-import "./components/css/product_responsive.css";
+import "./components/css/main_styles.css";
+import "./components/css/responsive.css";
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
