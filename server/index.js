b5ea0f2a6adbc1b197cdb60a82571f338f18abd6
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-20 16:02:02 -0400

    Testing CI/CD pipeline

--- server/index.js
+++ server/index.js
@@ -22,5 +22,14 @@ app.use(passport.session());
 
 require('./routes/authroutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = 9000;
 app.listen(PORT);
