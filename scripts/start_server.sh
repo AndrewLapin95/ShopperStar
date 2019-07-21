2793819782c54f67a1632b2e6ea67df3747f29e3
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-20 20:58:11 -0400

    Created docker stack in a detached mode

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,7 +6,7 @@ PROJECT_DIR="/var/www/shopperstar-project.com"
 cd $PROJECT_DIR
 kill $(ps aux | grep 'node' | ps aux | grep 'index.js' | awk '{print $2}')
 
-docker-compose --file docker-compose.yml up
+docker-compose --file docker-compose.yml up -d
 
 echo "Starting the application!"
 cd "$PROJECT_DIR/server"
