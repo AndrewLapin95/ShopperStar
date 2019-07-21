6d23a21d2595479a23a609aebbb04f05c52db557
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-20 20:51:55 -0400

    Updated start_server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,8 +4,10 @@ source ~/.env
 PROJECT_DIR="/var/www/shopperstar-project.com"
 
 cd $PROJECT_DIR
+kill $(ps aux | grep 'node' | ps aux | grep 'index.js' | awk '{print $2}')
+
 docker-compose --file docker-compose.yml up
 
 echo "Starting the application!"
 cd "$PROJECT_DIR/server"
-node index.js
+node index.js &
