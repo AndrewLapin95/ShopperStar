433871c8349adfe045792018ee41f4b74f81a3d3
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-20 23:37:16 -0400

    Updated script to start the server in a detached mode

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 source ~/.env
 
-kill $(ps aux | grep 'node' | ps aux | grep 'index.js' | awk '{print $2}') || true
-
 PROJECT_DIR="/var/www/shopperstar-project.com"
 cd $PROJECT_DIR
 
 docker-compose --file docker-compose.yml down
 docker-compose --file docker-compose.yml up -d
 
+sleep 90
+
 echo "Starting the application!"
 cd "$PROJECT_DIR/server"
-nohup node index.js &
+node index.js > /etc/shopperstar-project/stdout.txt 2> /etc/shopperstar-project/stderr.txt &
 
 exit 0
