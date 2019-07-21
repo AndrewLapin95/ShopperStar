89068f30c6832aff2699eeef914d8dbaef9e8859
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-20 22:40:11 -0400

    Updated script to start the server in a detached mode

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-rm -rf "/var/www/shopperstar-project.com/*"
+rm -rf "/var/www/shopperstar-project.com/*" || true
 kill $(ps aux | grep 'node' | ps aux | grep 'index.js' | awk '{print $2}') || true
+
+exit 0
