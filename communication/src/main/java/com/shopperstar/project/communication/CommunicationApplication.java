1376857e9d87af9e8e4668b822628caefc461f9f
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-21 20:10:10 -0400

    Initialize communication Spring Boot service

---
+++ communication/src/main/java/com/shopperstar/project/communication/CommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.shopperstar.project.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationApplication.class, args);
+	}
+
+}
