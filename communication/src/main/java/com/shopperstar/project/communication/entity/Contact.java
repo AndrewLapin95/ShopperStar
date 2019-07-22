cf616437cc48da6d54ea7742d4f9883a7b2a13a9
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-21 22:08:25 -0400

    Added fields, generated getters and settters, as well as toString methods for Contact and NewsletterSubscription entities

--- communication/src/main/java/com/shopperstar/project/communication/entity/Contact.java
+++ communication/src/main/java/com/shopperstar/project/communication/entity/Contact.java
@@ -1,5 +1,29 @@
 package com.shopperstar.project.communication.entity;
 
 public class Contact {
+	
+	private String userId;
+	private String email;
+	
+	public String getUserId() {
+		return userId;
+	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [userId=" + userId + ", email=" + email + "]";
+	}
+	
 }
