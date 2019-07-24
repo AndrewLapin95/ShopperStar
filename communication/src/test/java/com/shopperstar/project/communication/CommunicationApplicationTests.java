561696b7d7c0af96c93a17a1c7b386ba10ae0065
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-24 19:44:14 -0400

    Initialized communication service

---
+++ communication/src/test/java/com/shopperstar/project/communication/CommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopperstar.project.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
