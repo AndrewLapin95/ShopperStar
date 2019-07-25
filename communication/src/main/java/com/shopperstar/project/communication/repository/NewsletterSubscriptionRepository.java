14284bf94bdf8b4903a162903cf7554c5d92c779
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-24 21:07:45 -0400

    Added method signatures for NewsletterSubscriptionRepoistory interface

--- communication/src/main/java/com/shopperstar/project/communication/repository/NewsletterSubscriptionRepository.java
+++ communication/src/main/java/com/shopperstar/project/communication/repository/NewsletterSubscriptionRepository.java
@@ -1,10 +1,20 @@
 package com.shopperstar.project.communication.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.shopperstar.project.communication.model.NewsletterSubscription;
 
 public interface NewsletterSubscriptionRepository extends MongoRepository<NewsletterSubscription, String> {
-
+	
+	NewsletterSubscription save(NewsletterSubscription subscription);
+	
+	List<NewsletterSubscription> findAll();
+	
+	Optional<NewsletterSubscription> findById(String id);
+	
+	void deleteById(String id);
 	
 }
