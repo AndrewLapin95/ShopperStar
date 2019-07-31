474a3baa141c8e7a2527a538400758b788e4d68e
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-30 20:16:21 -0400

    Integrated communication service with the frontend

--- server/client/src/components/Newsletter.js
+++ server/client/src/components/Newsletter.js
@@ -1,10 +1,41 @@
-import React from "react"; 
+import React from "react";
+import axios from 'axios';
 
 class Newsletter extends React.Component { 
+    
     constructor(props) {
+
         super(props);
+        this.state = { email: "" }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
     } 
     
+    handleChange(event) {
+
+        this.setState({ [event.target.name]: event.target.value });
+
+    }
+
+    handleSubmit(event) {
+
+        event.preventDefault();
+
+        axios.post("/api/subscribe", {
+            
+            email: this.state.email
+
+        }).then(res => {
+            console.log(res);
+        });
+
+        this.setState({ 
+            email: ""
+         });
+
+    }
+
     render() { 
         return (
             <div className="newsletter">
@@ -22,8 +53,8 @@ class Newsletter extends React.Component {
                                     <p>{this.props.newsletterText}</p>
                                 </div>
                                 <div className="newsletter_form_container">
-                                    <form action="#" id="newsletter_form" className="newsletter_form">
-                                        <input type="email" className="newsletter_input" required="required" />
+                                    <form onSubmit={this.handleSubmit} id="newsletter_form" className="newsletter_form">
+                                        <input name="email" type="email" value={this.state.email} onChange={this.handleChange} className="newsletter_input" required="required"/>
                                         <button className="newsletter_button trans_200"><span>{this.props.newsletterButtonText}</span></button>
                                     </form>
                                 </div>
