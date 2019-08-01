c0d6b5ca57332fe4a8ec8c34584b082d662dc9f6
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-01 19:18:36 -0400

    Initialized checkout service

---
+++ checkout/src/test/java/com/shopperstar/project/checkout/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopperstar.project.checkout.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
