849a0fc55e3e3ab504b506ac69061f6007fe9e54
Andrey Lapin <lapinandrey95@gmail.com>
2019-07-31 20:29:29 -0400

    Initialized cart service

---
+++ cart/src/test/java/com/shopperstar/project/car/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopperstar.project.car.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
