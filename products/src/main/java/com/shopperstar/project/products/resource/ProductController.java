9d94d51c550382d1f1f21e240bc1f4773423357e
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-04 22:18:27 -0400

    Created resources for products service

---
+++ products/src/main/java/com/shopperstar/project/products/resource/ProductController.java
@@ -0,0 +1,97 @@
+package com.shopperstar.project.products.resource;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.shopperstar.project.products.model.Product;
+import com.shopperstar.project.products.repository.ProductRepository;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+	
+	@PostMapping("/api/create-product")
+	public ResponseEntity<Product> createProduct(@Valid @RequestBody Product newProduct) {
+		
+		Product savedProduct = null;
+		
+		try {
+			
+			logger.info("Creating new product:" + newProduct.getTitle());
+			savedProduct = repository.save(newProduct);
+			
+		} catch (Exception ex) {
+			
+			logger.error("Failed to create new product: " + newProduct.getTitle());
+			logger.error(ex.toString());
+		}
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+												  .buildAndExpand(savedProduct.getId())
+												  .toUri();
+		
+		return ResponseEntity.created(location).build();
+		
+	}
+	
+	@GetMapping("/api/get-products")
+	public List<Product> getAllProducts() {
+		return repository.findAll();
+	}
+	
+	@GetMapping("/api/get-product/{id}")
+	public Optional<Product> getProduct(@PathVariable String id) {
+		
+		Optional<Product> foundProduct = repository.findById(id);
+		
+		if (!foundProduct.isPresent()) {
+			throw new ProductNotFoundException("ID: " + id);
+		}
+		
+		return foundProduct;
+	}
+	
+	@DeleteMapping("/api/remove-product/{id}")
+	public ResponseEntity<Product> removeProduct(@PathVariable String id) {
+		
+		Product deletedProduct = getProduct(id).get();
+		
+		try {
+			
+			logger.info("Attempting to delete product: " + id);
+			repository.deleteById(id);
+			logger.info("Successfully deleted product with id: " + id);
+			
+		} catch (Exception ex) {
+			
+			logger.error("Failed to delete product: " + id);
+			logger.error(ex.toString());
+			
+		}
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .buildAndExpand(deletedProduct.getId())
+				  .toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+}
---
+++ products/src/main/java/com/shopperstar/project/products/resource/ProductNotFoundException.java
@@ -0,0 +1,14 @@
+package com.shopperstar.project.products.resource;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductNotFoundException extends RuntimeException {
+
+	public ProductNotFoundException(String message) {
+		super(message);
+	}
+
+}
