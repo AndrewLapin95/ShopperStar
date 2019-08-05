ec3500df81a34a47eed756a8ebebe646cc05d717
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-04 20:18:01 -0400

    Added custom exception handling

---
+++ communication/src/main/java/com/shopperstar/project/communication/resource/ContactNotFoundException.java
@@ -0,0 +1,13 @@
+package com.shopperstar.project.communication.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ContactNotFoundException extends RuntimeException {
+
+	public ContactNotFoundException(String message) {
+		super(message);
+	}
+
+}
---
+++ communication/src/main/java/com/shopperstar/project/communication/resource/NewsletterSubscriptionNotFound.java
@@ -0,0 +1,9 @@
+package com.shopperstar.project.communication.resource;
+
+public class NewsletterSubscriptionNotFound extends RuntimeException {
+
+	public NewsletterSubscriptionNotFound(String message) {
+		super(message);
+	}
+
+}
