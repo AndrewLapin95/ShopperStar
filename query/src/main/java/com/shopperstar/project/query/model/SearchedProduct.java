cdefff1b9662984ec1e01dd9686e32c7aa5a6335
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-08 19:41:19 -0400

    Created model and repository for query service

---
+++ query/src/main/java/com/shopperstar/project/query/model/SearchedProduct.java
@@ -0,0 +1,73 @@
+package com.shopperstar.project.query.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "products", type = "product")
+public class SearchedProduct {
+
+	@Id
+	private String id;
+	private String category;
+	private String image;
+	private Integer price;
+	private String title;
+	
+	public SearchedProduct() {};
+	
+	public SearchedProduct(String id, String category, String image, Integer price, String title) {
+		
+		this.id = id;
+		this.category = category;
+		this.image = image;
+		this.price = price;
+		this.title = title;
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchedProduct [id=" + id + ", category=" + category + ", image=" + image + ", price=" + price
+				+ ", title=" + title + "]";
+	}
+}
