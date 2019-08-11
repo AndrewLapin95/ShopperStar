94d86ac5b8e4a92ccb2703922e1ec1ae73c4065b
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-10 20:16:34 -0400

    Created query service

--- query/src/main/java/com/shopperstar/project/query/QueryApplication.java
+++ query/src/main/java/com/shopperstar/project/query/QueryApplication.java
@@ -2,6 +2,7 @@ package com.shopperstar.project.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
 public class QueryApplication {
@@ -9,5 +10,4 @@ public class QueryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
 	}
-
 }
---
+++ query/src/main/java/com/shopperstar/project/query/resource/QueryController.java
@@ -0,0 +1,46 @@
+package com.shopperstar.project.query.resource;
+
+import java.util.List;
+
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopperstar.project.query.model.SearchedProduct;
+import com.shopperstar.project.query.repository.ElasticSearchRepository;
+
+@RestController
+public class QueryController {
+	
+	@Autowired
+	private ElasticSearchRepository repository;
+	
+	@GetMapping("/api/get-products")
+	public List<SearchedProduct> getAllProducts() {
+		return repository.findAll();
+	}
+	
+	@GetMapping("/api/get-products-by-category/{category}")
+	public List<SearchedProduct> getProductsByCategory(@PathVariable String category) {
+		QueryBuilder query = QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("category", category.toUpperCase()));
+		return repository.search(query);
+	}
+	
+	@GetMapping("/api/get-products-by-title/{title}")
+	public List<SearchedProduct> getProductsByTitle(@PathVariable String title) {
+		QueryBuilder query = QueryBuilders.commonTermsQuery("title", title);
+		return repository.search(query);
+	}
+	
+	@GetMapping("/api/search-products/{entry}")
+	public List<SearchedProduct> searchProducts(@PathVariable String entry) {
+		QueryBuilder query = QueryBuilders.boolQuery()
+				                          .should(QueryBuilders.commonTermsQuery("title", entry))
+				                          .should(QueryBuilders.commonTermsQuery("category", entry))
+				                          .minimumShouldMatch(1);
+		return repository.search(query);
+	}
+}
