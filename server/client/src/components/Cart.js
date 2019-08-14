97ee8f228806a3ad9ba6e196a457734bfb70e0d8
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-14 01:50:51 -0400

    Started working on integrating the cart service

--- server/client/src/components/Cart.js
+++ server/client/src/components/Cart.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import DeliveryPanel from "./DeliveryPanel";
 import CartTotal from "./CartTotal";
-import CartSummarisation from "./CartSummarisation"; 
+import CartSummarisation from "./CartSummarisation";
+import axios from "axios";
 
 class Cart extends React.Component { 
+    
+    constructor(props) {
+        
+        super(props);
+
+        this.state = {
+            cart: {}
+        }
+    }
+
+    async componentDidMount() {
+
+        const user = await axios.get("/api/current_user/");
+        const createCart = await axios.post("/api/create-cart/" + user.data._id);
+        const cart = await axios.get("/api/get-cart/" + user.data._id);
+
+        this.setState({
+            cart: cart.data
+        });
+
+        console.log(this.state.cart);
+    }
+    
     render() { 
         return (
-            <div class="super_container">
-                <div class="cart_info">
-                    <div class="container">
+            <div className="super_container">
+                <div className="cart_info">
+                    <div className="container">
                         <CartSummarisation />
-                        <div class="row row_extra">
-                            <div class="col-lg-4">
+                        <div className="row row_extra">
+                            <div className="col-lg-4">
                                 <DeliveryPanel />
                             </div>
-                            <div class="col-lg-6 offset-lg-2">
+                            <div className="col-lg-6 offset-lg-2">
                                 <CartTotal />
                             </div>
                         </div>
